1f16e33dbe76d49b9d4836666f96b52df3249ec3
Ahir Zenbook <ahirchatterjee@gmail.com>
2020-07-13 18:11:09 -0500

    completed xpPerMessage implementation, along with giveXp. !giveXp is still a work in progress

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 import os
 import json
 import pymysql
-import datetime    
+import datetime 
 
 client = discord.Client()
 
@@ -37,45 +37,48 @@ async def checkCommands(message):
                     giveXp(user,amount,False)
         permissionDenied(message,channel)
 
-def giveXp(user,amount,timePenalty):
+def giveXp(userId,amount,timePenalty):
     timeFormat = '%Y-%m-%d %H:%M:%S'
-    cursor.execute("SELECT * FROM users WHERE discordId = \"" + author + "\"")
+    cursor.execute("SELECT * FROM users WHERE discordId = \"" + userId + "\"")
     result = cursor.fetchall()
     assert not len(result) > 1, "more than one entry with the same discordId"
-
-    timeFormat = '%Y-%m-%d %H:%M:%S'
-    cursor.execute("SELECT * FROM users WHERE discordId = \"" + author + "\"")
-    result = cursor.fetchall()
 
     now = datetime.datetime.now()
     ts = now.strftime(timeFormat)
     elapsedMins = 0
+    xp = amount
 
-    if(len(result) == 0): #if this is a new user, create a new entry with 1 xp in the database
+    if(len(result) == 0): #if this is a new user
+        if(not timePenalty):
+            ts = ts #hacky way to make sure there's no time penalty after this one
         formatStr = """
             INSERT INTO users (`discordId`, `xp`, `lastUpdated`)
             VALUES ("{dId}",{exp},"{time}");
             """
-        cursor.execute(formatStr.format(dId=author,exp=xp,time=ts))
-    else: #if this is a returning user, increment 1 xp into the database if it has been at least a minute
-        xp = result[0][1]
+        cursor.execute(formatStr.format(dId=userId,exp=xp,time=ts))
+    else: #if this is a returning user
+        xp += result[0][1]
         then = result[0][2]
-        elapsedMins = now.minute - then.minute
-        print("This user was last updated " + (str)(elapsedMins) + " minutes ago!")
-        if(elapsedMins > 0):
-            xp += 1
-            cursor.execute("UPDATE users SET xp = " + (str)(xp) + ", lastUpdated = \"" + ts + "\" WHERE discordId = \"" + author + "\"")   
-            DB.commit()
+        if(not timePenalty):
+            ts = then.strftime(timeFormat)
+        else:
+            elapsedMins = now.minute - then.minute
+            print("This user was last updated " + (str)(elapsedMins) + " minutes ago!")
+            if(elapsedMins > 0):
+                return xp
+        cursor.execute("UPDATE users SET xp = " + (str)(xp) + ", lastUpdated = \"" + ts + "\" WHERE discordId = \"" + userId + "\"")   
+        DB.commit()
+    return xp
            
 async def permissionDenied(message,channel):
     await channel.send("You do not have permission for this command.")
 
 async def xpPerMessage(message):
-    xp = giveXp(author,xp,True)    
+    author = (str)(message.author.id)
+    xp = giveXp(author,1,True)    
     milestoneCounter = 2
     if(xp % milestoneCounter == 0): #tracking milestones (increments of 5 for debug purposes)
         await message.channel.send("<@" + author + "> has " + (str)(xp) + " xp!")
-    print("Author: " + author + " has " + (str)(xp) + " xp and was updated " + (str)(elapsedMins) + " minutes ago!")
 
 @client.event
 async def on_message(message):
@@ -84,7 +87,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     else:
-        await handleXp(message)
+        await xpPerMessage(message)
     if message.content.startswith('Hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await channel.send(msg)
