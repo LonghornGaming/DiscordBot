d42047c17b5d93decff4b7e8c98535cfb915d7fd
Bml829 <76921672+Bml829@users.noreply.github.com>
2021-07-15 19:39:16 -0500

    add tiers embed (#23)

--- cogs/xp.py
+++ cogs/xp.py
@@ -29,9 +29,14 @@ class XP(commands.Cog):
     """
     @commands.command(name='tiers')
     async def showTiers(self, ctx):
-        embed=discord.Embed(title="XP Tier List!")
+        embed=discord.Embed(title="Bevo Bot's XP Tiers", description="All XP tiers come with their own respective Discord roles. ", color=0xbf5700)
+        embed.add_field(name="Tier 1: Bronze, 500 XP", value="LG member t-shirt\n\u200B", inline=False)
+        embed.add_field(name="Tier 2: Silver, 2000 XP", value="LG (small) sticker\n\u200B", inline=False)
+        embed.add_field(name="Tier 3: Gold, 5000 XP", value="LG (large) sticker\n\u200B", inline=False)
+        embed.add_field(name="Tier 4: Platinum, 10000 XP", value="LG Holographic sticker\n\u200B", inline=False)
+        embed.add_field(name="Tier 5: Diamond, 20000 XP", value="LG gamer sticker\n\u200B", inline=False)
+        embed.set_footer(text="All in-kind prizes are for LG members only. Prizes are tentative and subject to change. If we add a prize but you’ve already surpassed that rank, you’ll still receive it retroactively.")
         await ctx.send(embed=embed)
-        return
     
 
     """
